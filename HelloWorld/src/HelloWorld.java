21a3e89324d366e2b77ca55da168d921c2759c17
jalexand <jalexand@VPN-JALEXAND.CHRR.local>
2018-07-19 16:16:24 -0400

    my very first commit to github

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * A simple hello world utility class to test github repo.
+ * @author gavin bellson
+ *
+ */
+
+public class HelloWorld {
+	/**
+	 * Private constructor so this class cannot be instantiated
+	 * 
+	 */
+
+	public static void  main(String args[]) {
+		System.out.println("hello world");
+	}
+}
