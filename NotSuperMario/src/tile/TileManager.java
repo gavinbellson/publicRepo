d63d195190416eaf02a6349510d60bdbab1cba6e
Jeremy <Jeremy@DESKTOP-N3CL5OI>
2023-12-31 11:20:54 -0500

    got first single screen map working!

--- NotSuperMario/src/tile/TileManager.java
+++ NotSuperMario/src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -12,12 +15,15 @@ public class TileManager {
 	/* class variables */
 	GamePanel gamePanel;
 	Tile[] tile;
+	int mapTileNum [] [];
 
 	/* constructor */
 	public TileManager (GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 		tile = new Tile[10];
+		mapTileNum = new int [gamePanel.maxScreenCol] [gamePanel.maxScreenRow];
 		getTileImage();
+		loadMap("/maps/map01.txt");
 	}
 	
 	/* get tile image method */
@@ -31,11 +37,41 @@ public class TileManager {
 			
 			tile[2] = new Tile();
 			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+			
+			tile[3] = new Tile();
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/tree.png"));
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	/* map loader */
+	public void loadMap(String mapFilePath) {
+		try {
+			InputStream inputStream = getClass().getResourceAsStream(mapFilePath);
+			BufferedReader br = new BufferedReader (new InputStreamReader(inputStream));
+			int col = 0;
+			int row = 0;
+			while (col < gamePanel.maxScreenCol && row < gamePanel.maxScreenRow) {
+				String line = br.readLine();
+				while (col < gamePanel.maxScreenCol) {
+					String numbers [] = line.split(" ");
+					int num = Integer.parseInt(numbers[col]);
+					mapTileNum[col][row] = num;
+					col++;
+				}
+				if (col ==gamePanel.maxScreenCol) {
+					col = 0;
+					row++;
+				}
+			}
+			br.close();//close bufferedreader
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/* draw method */
@@ -47,7 +83,10 @@ public class TileManager {
 		int y = 0;
 		
 		while (col < gamePanel.maxScreenCol && row < gamePanel.maxScreenRow) {
-			g2.drawImage (tile[0].image,x,y,gamePanel.tileSize,gamePanel.tileSize,null);
+			int tileNum = mapTileNum [col] [row] ;
+			System.out.println("tileNum = " + tileNum);
+			
+			g2.drawImage (tile[tileNum].image,x,y,gamePanel.tileSize,gamePanel.tileSize,null);
 			col++;
 			x = x + gamePanel.tileSize;
 			
